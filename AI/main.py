3e4c05e5347c1c1cecf876b3864189bcf5d183dc
KaliedRuby <1700011094@pku.edu.cn>
2018-06-03 16:21:25 +0800

    新的main与func接口，时间计量

--- AI/main.py
+++ AI/main.py
@@ -1,20 +1,25 @@
 def play(stat, storage):
-    ''' 我们现在来实验，从网上下载 '''
+    from time import time
     from AI.DSA_group1_package.find_path import path_to
     from AI.DSA_group1_package import gen_func
+
+    timetemp = [stat['now']['turnleft'][0]]
+    print(timetemp)
+    t = time()
+    t0 = int(round(t * 1000))
     # 距离计算的记录，一次比赛的play完成后会删除
     storage['path'] = {'me': {'me': [None], 'enemy': [None, None]},
                        'enemy': {'enemy': [None], 'me': [None, None]}, }
     storage['fields_count'] = None
 
     safe_point, chance_point = 0, 0
-    name1 = stat['me']['id']
-    name2 = stat['me']['id']
-    pos_me = (stat['me']['x'], stat['me']['y'])
-    pos_enemy = (stat['enemy']['x'], stat['enemy']['y'])
+    name1 = stat['now']['me']['id']
+    name2 = stat['now']['me']['id']
+    pos_me = (stat['now']['me']['x'], stat['now']['me']['y'])
+    pos_enemy = (stat['now']['enemy']['x'], stat['now']['enemy']['y'])
 
     # 计算安全感指数与胜机指数
-    if stat['fields'][pos_me[0]][pos_me[1]] == name1:  # 我在自己领地，安全感+++
+    if stat['now']['fields'][pos_me[0]][pos_me[1]] == name1:  # 我在自己领地，安全感+++
         safe_point += 90
     # 我回领地难于对方进攻，安全感*-
     elif path_to(stat, storage, 'me', 'me', 'fields')['dis'] > path_to(stat, storage, 'enemy', 'me', 'bands')['dis']:
@@ -24,33 +29,46 @@ def play(stat, storage):
         safe_point -= 3 * (path_to(stat, storage, 'me', 'me', 'fields')['dis'] -
                            path_to(stat, storage, 'enemy', 'me', 'bands')['dis'])
 
-    if stat['fields'][pos_me[0]][pos_me[1]] == name2:  # 我进入敌方领地，安全感--
+    if stat['now']['fields'][pos_me[0]][pos_me[1]] == name2:  # 我进入敌方领地，安全感--
         safe_point -= 20
 
     temp = gen_func.fields_count(stat, storage)
-    safe_point += 2 * (temp[0] - temp[1])  # 领地越大越安全，安全感*（+/-）
-    if stat['fields'][pos_enemy[0]][pos_enemy[1]] == name2:  # 敌方回领地，胜机---
+    safe_point += 0.5 * (temp[0] - temp[1])  # 领地越大越安全，安全感*（+/-）
+
+    t = time()
+    t2 = int(round(t * 1000))
+    timetemp.append(['safe_calculate:', t2 - t0])
+
+    if stat['now']['fields'][pos_enemy[0]][pos_enemy[1]] == name2:  # 敌方回领地，胜机---
         chance_point -= 60
     elif path_to(stat, storage, 'me', 'enemy', 'bands')['dis'] < \
             path_to(stat, storage, 'enemy', 'enemy', 'fields')['dis']:  # 我进攻易于敌退守，胜机++，*+
         chance_point += 10 - 5 * (path_to(stat, storage, 'me', 'enemy', 'bands')['dis'] -
                                   path_to(stat, storage, 'enemy', 'enemy', 'fields')['dis'])
     else:  # 我进攻难于敌退守，胜机*-
-        chance_point -= 10 * (path_to(stat, storage, 'me', 'enemy', 'bands')['dis'] -
+        chance_point -= 5 * (path_to(stat, storage, 'me', 'enemy', 'bands')['dis'] -
                               path_to(stat, storage, 'enemy', 'enemy', 'fields')['dis'])
-    if stat['fields'][pos_enemy[0]][pos_enemy[1]] == name1:  # 敌进入我方领地，胜机++
+    if stat['now']['fields'][pos_enemy[0]][pos_enemy[1]] == name1:  # 敌进入我方领地，胜机++
         chance_point += 20
     else:
         chance_point += 1 * path_to(stat, storage, 'enemy', 'me', 'fields')['dis']
 
+    t = time()
+    t3 = int(round(t * 1000))
+    timetemp.append(['chance_calculate:', t3 - t2])
+
     # 如果可以立即获胜，那么进行
     for key, value in gen_func.direction_dict.items():
-        new = gen_func.move(stat['me']['x'], stat['me']['y'], value + stat['me']['direction'])
-        if new[0] < 0 or new[1] < 0 or new[0] >= storage['size'][0] or new[1] >= storage['size'][1]:
+        new = gen_func.move(stat['now']['me']['x'], stat['now']['me']['y'], value + stat['now']['me']['direction'])
+        if new[0] < 0 or new[1] < 0 or new[0] >= stat['size'][0] or new[1] >= stat['size'][1]:
             continue
         if gen_func.win_check(stat, storage, *new):
             storage['memory'].append('Win Immediately')
+            t = time()
+            t1 = int(round(t * 1000))
+            timetemp.append(['immediately:', t1 - t3])
             # storage['path'] = None
+            storage['time'].append([storage['nowevent'].name, timetemp])
             return key
 
     # 之前的事件是否延续
@@ -64,12 +82,17 @@ def play(stat, storage):
             if not pastevent.event_break_check(stat, storage, safe_point, chance_point):
                 temp = pastevent.event_continue(stat, storage)
                 if storage['memory'] == []:
-                    storage['memory'].append((pastname, 1))
-                elif pastname == storage['memory'][len(storage['memory'])-1][0]:
-                    storage['memory'][1] += 1
+                    storage['memory'].append([pastname, 1])
+                elif pastname == storage['memory'][-1][0]:
+                    storage['memory'][-1][1] += 1
                 else:
-                    storage['memory'].append((pastname , 1))
+                    print(storage['memory'][-1])
+                    storage['memory'].append([pastname , 1])
                 # storage['path'] = None
+                t = time()
+                t1 = int(round(t * 1000))
+                timetemp.append(['fun_continue:',t1-t3])
+                storage['time'].append([storage['nowevent'].name, timetemp])
                 return temp
             else:
                 pastevent.event_stop(storage)
@@ -82,19 +105,24 @@ def play(stat, storage):
     else:
         output = storage['event']['Defend'].event_start(stat, storage, pastname)
 
+    t = time()
+    t4 = int(round(t * 1000))
+    timetemp.append(['newevent:', t4 - t3])
+
     if storage['memory'] == []:
-        storage['memory'].append((storage['nowevent'].name, 1))
-    elif storage['nowevent'].name == storage['memory'][len(storage['memory']) - 1][0]:
-        storage['memory'][1] += 1
+        storage['memory'].append([storage['nowevent'].name, 1])
+    elif storage['nowevent'].name == storage['memory'][- 1][0]:
+        storage['memory'][-1][1] += 1
     else:
-        storage['memory'].append((storage['nowevent'].name, 1))
+        print(storage['memory'][-1])
+        storage['memory'].append([storage['nowevent'].name, 1])
     # storage['path'] = None
+    storage['time'].append([storage['nowevent'].name,timetemp])
     return output
 
 
-# load函数，初始化storage
-def load(storage):
-    import random
+# init函数，初始化storage
+def load(stat , storage):
 
     def acfun1(stat, storage, name):
         from AI.DSA_group1_package.attack import attack
@@ -103,7 +131,7 @@ def load(storage):
 
     def acfun2(stat, storage, name):
         from AI.DSA_group1_package.retreat import retreat
-        return retreat(stat=stat, storage=storage)
+        return retreat(stat=stat, storage=storage , order = name)
         # return random.choice('LMR')
 
     def acfun3(stat, storage, name):
@@ -113,14 +141,18 @@ def load(storage):
 
     from AI.DSA_group1_package.event_class import Event
 
-    storage['memory'] = []
-
     temp = {'Attack': Event('Attack', acfun1), 'Defend': Event('Defend', acfun2),
             'Expand': Event('Expand', acfun3)}
     storage['event'] = temp
-    storage['nowevent'] = 'first'
 
-def summary(match_result , storage):
+    storage['memory'] = []
+    storage['nowevent'] = 'first'
+    storage['time'] = []
+
+#对局总结函数(单次)
+def summary(match_result , stat , storage):
     print(match_result)
     print(storage['memory'])
+    print(storage['time'])
     storage['memory'] = None
+    storage['path'] = None
