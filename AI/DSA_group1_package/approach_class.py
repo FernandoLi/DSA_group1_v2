72955b1c1fae34826403296db05756a2d610fbb4
KaliedRuby <39729983+KaliedRuby@users.noreply.github.com>
2018-06-09 00:44:27 +0800

    更改决策方式，fieldscount调整

--- AI/DSA_group1_package/approach_class.py
+++ AI/DSA_group1_package/approach_class.py
@@ -45,7 +45,6 @@ class Approach(State):
                 return method
 
     def init_output(self, stat, storage, last_stat_name):
-        print(2000-stat['now']['turnleft'][0],'approach')
         if me_and_enemy_easy(stat,storage)>2 and to_enemy_fields_easy(stat['now']['me']['x'] , stat['now']['me']['y'],stat,storage)>3:
             storage['approach_type'] = 'Attack'
             return self.approach_wrapper(stat,storage,self.init_output_attack(stat, storage, last_stat_name))
@@ -54,37 +53,32 @@ class Approach(State):
             return self.approach_wrapper(stat,storage,self.init_output_invade(stat, storage))
 
     def init_output_invade(self, stat, storage):
-        def get_surrounding(inx,iny):
-            return ((inx-1,iny),(inx+1,iny),(inx,iny-1),(inx,iny+1),(inx-1,iny-1),(inx+1,iny+1),(inx-1,iny+1),(inx+1,iny-1))
-        flag ,max1,pointer1= 0,0,0
-        max2,pointer2 = 0,0
-        for x in range(stat['size'][0]):
-            for y in range(stat['size'][1]):
-                if stat['now']['fields'][x][y] == stat['now']['me']['id']:
-                    surr = get_surrounding(x,y)
-                    surr_num = 0
-                    xy_dis = abs(x - stat['now']['enemy']['x']) + abs(y - stat['now']['me']['y'])
-                    for element in surr:
-                        if stat['now']['fields'][element[0]][element[1]] == stat['now']['enemy']['id']:
-                            surr_num += 1
-                    if surr_num > max1:
-                        max1 = surr_num
-                        pointer1 = (x,y)
-                    if xy_dis > max2:
-                        max2 = xy_dis
-                        pointer2 = (x,y)
-        if pointer1 == 0:
-            storage['pointer'] = pointer2
-        else:
-            storage['pointer'] = pointer1
-        left = move(stat['now']['me']['x'], stat['now']['me']['y'], direction_dict[LEFT])
-        right = move(stat['now']['me']['x'], stat['now']['me']['y'], direction_dict[RIGHT])
-        middle = move(stat['now']['me']['x'], stat['now']['me']['y'], direction_dict[MIDDLE])
-        choice = []
-        choice.append([LEFT,abs(left[0] - storage['pointer'][0]) + abs(left[1] - storage['pointer'][1])])
-        choice.append([RIGHT, abs(right[0] - storage['pointer'][0]) + abs(right[1] - storage['pointer'][1])])
-        choice.append([MIDDLE, abs(middle[0] - storage['pointer'][0]) + abs(middle[1] - storage['pointer'][1])])
-        return min(*choice,key=lambda x:x[1])[0]
+        choose = [LEFT,RIGHT,MIDDLE]
+        count  = [[0,0,0], [0,0,0],[0,0,0]]
+        min = [999,999]
+        choose_list = []
+        mindis = None
+        for i in range(0,3):
+            newx,newy = stat['now']['me']['x'],stat['now']['me']['y']
+            while newx>0 and newy >0 and newx<stat['size'][0]and newy<stat['size'][1]:
+                newx,newy = move(newx, newy, direction_dict[choose[i]] + stat['now']['me']['direction'])
+                if stat['now']['fields'][newx][newy] != stat['now']['me']['id']:
+                    count[i][1] = count[i][0]
+                    count[i][2] = abs(newx - stat['now']['enemy']['x']) + abs(newy - stat['now']['me']['y'])
+                    if count[i][2] < min[0]:
+                        min[0] = count[i][2]
+                        mindis = choose[i]
+                    choose_list.append((choose[i],count[i][1]))
+                    break
+                count[i][0] += 1
+        choose = [x for x in choose_list if x[0] != mindis]
+        min = [999,MIDDLE]
+        for element in choose:
+            if element[1]<min[0]:
+                min[0] = element[1]
+                min[1] = element[0]
+        return min[1]
+
 
 
 
@@ -116,27 +110,13 @@ class Approach(State):
                     return LEFT
 
     def subquent_output(self , stat ,storage):
-        print(2000-stat['now']['turnleft'][0],'approach')
         if storage['approach_type'] == 'Attack':
             return self.approach_wrapper(stat,storage,self.subquent_output_attack(stat, storage))
         else:
             return self.approach_wrapper(stat,storage,self.subquent_output_invade(stat,storage))
 
     def subquent_output_invade(self , stat , storage):
-        if (stat['now']['me']['x'], stat['now']['me']['y']) == storage['pointer']:
-            nowx ,nowy = stat['now']['me']['x'], stat['now']['me']['y']
-            for pos in[(nowx-1,nowy),(nowx+1,nowy),(nowx,nowy-1),(nowx,nowy+1)]:
-                if stat['now']['fields'][pos[0]][pos[1]] != stat['now']['me']['id']:
-                    storage['pointer'] = pos
-        else:
-            left = move(stat['now']['me']['x'], stat['now']['me']['y'], direction_dict[LEFT])
-            right = move(stat['now']['me']['x'], stat['now']['me']['y'], direction_dict[RIGHT])
-            middle = move(stat['now']['me']['x'], stat['now']['me']['y'], direction_dict[MIDDLE])
-            choice = []
-            choice.append([LEFT, abs(left[0] - storage['pointer'][0]) + abs(left[1] - storage['pointer'][1])])
-            choice.append([RIGHT, abs(right[0] - storage['pointer'][0]) + abs(right[1] - storage['pointer'][1])])
-            choice.append([MIDDLE, abs(middle[0] - storage['pointer'][0]) + abs(middle[1] - storage['pointer'][1])])
-            return min(*choice, key=lambda x: x[1])[0]
+        return MIDDLE
 
 
     def subquent_output_attack(self, stat, storage):
